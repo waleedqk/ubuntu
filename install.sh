95f56f8dc9c056a0f999996684e33abde68aee25
Waleed <wqkhan@uwaterloo.ca>
2017-01-23 03:16:00 +0000

    install.sh created online with Bitbucket

---
+++ install.sh
@@ -0,0 +1,5 @@
+wget http://cdn01.foxitsoftware.com/pub/foxit/reader/desktop/linux/2.x/2.1/en_us/FoxitReader2.1.0805_Server_x64_enu_Setup.run.tar.gz
+tar xzvf FoxitReader*.tar.gz
+sudo chmod a+x FoxitReader*.run
+./FoxitReader.*.run
+sudo ./FoxitReader.*.run
