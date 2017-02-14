70946b51eb9ac8179561825929be2d9bdf627bbe
waleed <wqkhan@uwaterloo.ca>
2017-02-13 21:56:27 -0500

    Commit

--- scripts/ubuntu_14_04_install.sh
+++ scripts/ubuntu_14_04_install.sh
@@ -8,7 +8,6 @@ add_ppa()
 	add-apt-repository ppa:gnome-terminator -y
 	add-apt-repository ppa:eugenesan/ppa -y
 	apt-add-repository ppa:blahota/texstudio -y
-	add-apt-repository ppa:pipelight/stable -y
 }
 
 apt_update()
@@ -20,17 +19,12 @@ apt_update()
 install_packages()
 {
 	echo "Installing packages"
-	apt-get install build-essential ubuntu-restricted-extras synaptic exfat-fuse exfat-utils gcc g++ openssh-server openssh-client git xclip terminator texlive-full mc texstudio vlc vim wireshark libnss3* --install-recommends pipelight-multi -y 
+	apt-get install build-dep build-essential ubuntu-restricted-extras synaptic exfat-fuse exfat-utils gcc g++ openssh-server openssh-client git xclip terminator texlive-full mc texstudio vlc vim wireshark libnss3* -y 
 }
 
 #add_ppa
 apt_update
 install_packages
-
-sudo pipelight-plugin --update
-sudo pipelight-plugin --enable silverlight
-# sudo pipelight-plugin --disable silverlight --enable silverlight5.0
-sudo pipelight-plugin --create-mozilla-plugins 
 
 echo "Finished adding PPAs and insatlling applications"
 exit 0
