a33671e79347e80b8bfc97238a3e39b618766182
Waleed <wqkhan@uwaterloo.ca>
2017-02-23 19:46:56 -0500

    Commit

---
+++ scripts/ubuntu_16_04_install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+add_ppa()
+{
+	echo "Adding PPAs"
+	add-apt-repository ppa:gnome-terminator -y
+	add-apt-repository ppa:eugenesan/ppa -y
+	apt-add-repository ppa:blahota/texstudio -y
+}
+
+apt_update()
+{
+	echo "updating repositories"
+	apt-get update
+}
+
+install_packages()
+{
+	echo "Installing packages"
+	apt-get install build-dep build-essential ubuntu-restricted-extras synaptic exfat-fuse exfat-utils gcc g++ openssh-server openssh-client git xclip terminator texlive-full mc texstudio vlc vim wireshark libnss3* -y 
+}
+
+#add_ppa
+apt_update
+install_packages
+
+echo "Finished adding PPAs and insatlling applications"
+exit 0
+
