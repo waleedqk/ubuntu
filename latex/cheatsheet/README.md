75d9b19cde49d1bfeacb2997a1f10b4d962a84f1
wqkhan <wqkhan@uwaterloo.ca>
2017-03-02 17:36:50 -0500

    Commit

---
+++ latex/cheatsheet/README.md
@@ -0,0 +1,23 @@
+# Ubuntu Cheatsheet
+This is the source of the Ubuntu Cheatsheet.
+
+## Making your own copy/variation
+
+### Requirements
+
+Building this document requires installing pdflatex
+
+On ubuntu you can get that with this command:
+```
+sudo apt-get install texlive-latex-base
+```
+
+### Make your changes
+
+Edit *.tex
+
+### Build the outputs
+
+To build the pdfs just type `make`
+
+
--- scripts/ubuntu_14_04_install.sh
+++ scripts/ubuntu_14_04_install.sh
@@ -4,12 +4,13 @@ echo "Adding PPAs"
 add-apt-repository ppa:gnome-terminator -y
 add-apt-repository ppa:eugenesan/ppa -y
 apt-add-repository ppa:blahota/texstudio -y
+sudo add-apt-repository ppa:diesch/testing
 
 echo "updating repositories"
 apt-get update
 
 echo "Installing packages"
-apt-get install build-essential ubuntu-restricted-extras synaptic exfat-fuse exfat-utils gcc g++ openssh-server openssh-client git xclip terminator texlive-full mc texstudio vlc -y 
+apt-get install build-essential ubuntu-restricted-extras synaptic alacarte classicmenu-indicator exfat-fuse exfat-utils gcc g++ openssh-server openssh-client git xclip terminator texlive-full mc texstudio vlc -y 
 
 echo "Finished adding PPAs and insatlling applications"
 exit 0
