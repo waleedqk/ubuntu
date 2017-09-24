aa4adbcac0e78a04ea41645093abd5a83b9f2061
Waleed Khan <wqkhan@uwaterloo.ca>
2017-09-23 23:56:56 -0400

    Commit

--- scripts/install_script.bash
+++ scripts/install_script.bash
@@ -39,14 +39,6 @@ install_gchrome()
 	sudo apt-get install google-chrome-stable -y
 }
 
-install_sublimetext()
-{
-	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-	sudo apt-get update
-	sudo apt-get install sublime-text
-}
-
 
 echo "Apt Update"
 apt_update
@@ -57,9 +49,8 @@ then
 	echo "Ubuntu 16.04 detected"
 	#Run this function
 	#add_16_ppa
-	#install_16_packages
+	install_16_packages
 	#install_gchrome
-	install_sublimetext
 
 elif [[ `lsb_release -rs` == "14.04" ]] # for Ubuntu 14.04
 then
