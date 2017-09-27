2946ee891cac00ae557ece0fc4ba5a04d82593dd
waleedqk <wqkhan@uwaterloo.ca>
2017-09-27 10:23:36 -0400

    Commit

--- scripts/install_script.bash
+++ scripts/install_script.bash
@@ -2,33 +2,123 @@
 
 set -e
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+REPO_DIR="$(dirname "$SCRIPT_DIR")"
+
+UBUNTU_CODENAME=$(lsb_release -s -c)	#Xenial
+UBUNTU_VERSION=$(lsb_release -rs)	#16.04
+
+# APP LIST
+APP_LIST=(
+	build-essential
+	cifs-utils
+	cmake
+	cron
+	dconf-editor
+	dkms
+	docker-ce
+	espeak
+	exfat-fuse
+	exfat-utils
+	expect
+	g++
+	gcc
+	git
+	gksu
+	gparted
+	gzip
+	handbrake-gtk 
+	handbrake-cli
+	htop
+	i3
+	inkscape
+	mc
+	minecraft-installer
+	nmap
+	ntp
+	openssh-client
+	openssh-server
+	openvpn
+	python-pip
+	python3-pip
+	screen
+	shutter
+	spotify-client
+	sqlite3
+	ssh
+	steam
+	synaptic
+	syncthing
+	tcpdump
+	#terminator
+	telnet
+	texlive-full
+	texstudio
+	tree
+	tmux
+	ubuntu-restricted-extras
+	vim
+	virtualbox
+	vlc
+	wireshark
+	xclip
+	youtube-dl
+)
+
+main_16()
+{
+	apt_update
+	clear
+	#add_16_ppa
+
+	echo "Installing apps now ..."
+	sudo apt-get -y install "${APP_LIST[@]}"
+
+	echo ""	
+	#install_gchrome
+	#configure_16
+	#install_ros
+
+}
+
 apt_update()
 {
-	echo "updating repositories"
+	echo "update..."
 	apt-get update
 }
 
 add_16_ppa()
 {
 	echo "Adding PPAs"
+	sudo add-apt-repository "deb http://archive.canonical.com/ubuntu $(lsb_release -sc) partner"
+	
 	# terminator terminal
 	#add-apt-repository ppa:gnome-terminator/ppa -y
+
 	# open source video transcoder
-	#add-apt-repository ppa:stebbins/handbrake-releases -y
+	add-apt-repository ppa:stebbins/handbrake-releases -y
+
 	# shutter
-	#add-apt-repository ppa:shutter/ppa
+	add-apt-repository ppa:shutter/ppa
 
+	# minecraft
+	sudo add-apt-repository -y ppa:minecraft-installer-peeps/minecraft-installer
 
-	# Not Used	
-	#add-apt-repository ppa:eugenesan/ppa -y
-}
+	# Syncthing
+	curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
+	echo "deb https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
 
-install_16_packages()
-{
-	# need to update to include added PPA
-	apt_update
-	echo "Installing packages"
-	apt-get install build-essential ubuntu-restricted-extras synaptic exfat-fuse exfat-utils cron cmake gcc g++ openssh-server openssh-client git i3 gparted ntp screen gksu dkms gzip cifs-utils xclip terminator tmux shutter sqlite3 texlive-full mc texstudio vlc vim wireshark tcpdump telnet handbrake-gtk handbrake-cli espeak youtube-dl dconf-editor -y 
+	# Docker
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+	# Spotify
+	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+	echo 'deb http://repository.spotify.com stable non-free' | sudo tee /etc/apt/sources.list.d/spotify.list
+
+	echo "Updating package lists ..."
+	sudo apt-get update -qq
+
 }
 
 configure_16()
@@ -48,25 +138,50 @@ install_gchrome()
 }
 
 
-echo "Apt Update"
+install_ros()
+{
+	UBUNTU_CODENAME=$(lsb_release -s -c)
+	case $UBUNTU_CODENAME in
+		trusty)
+			ROS_DISTRO=indigo;;
+		xenial)
+			ROS_DISTRO=kinetic;;
+		*)
+			echo "Unsupported version of Ubuntu detected. Only trusty (14.04.*) and xenial (16.04.*) are currently supported."
+		exit 1
+	esac
+
+	sudo sh -c "echo \"deb http://packages.ros.org/ros/ubuntu $UBUNTU_CODENAME main\" > /etc/apt/sources.list.d/ros-latest.list"
+	wget -qO - http://packages.ros.org/ros.key | sudo apt-key add -
+
+	echo "Updating package lists ..."
+	sudo apt-get -qq update
+
+	echo "Installing ROS $ROS_DISTRO ..."
+	sudo apt-get -qq install ros-$ROS_DISTRO-desktop python-rosinstall > /dev/null
+
+	source /opt/ros/$ROS_DISTRO/setup.bash
+
+	# Prepare rosdep to install dependencies.
+	echo "Updating rosdep ..."
+	if [ ! -d /etc/ros/rosdep ]; then
+		sudo rosdep init > /dev/null
+	fi
+	rosdep update > /dev/null
+}
+
+# Update System
 apt_update
 
-if [[ `lsb_release -rs` == "16.04" ]] # for Ubuntu 16.04
-then
+case $UBUNTU_CODENAME in
+	trusty)
+		main_14;;
+	xenial)
+		main_16;;
+	*)
+		echo "Unsupported version of Ubuntu detected. Only trusty (14.04.*) and xenial (16.04.*) are currently supported."
+	exit 1
+esac
 
-	echo "Ubuntu 16.04 detected"
-	#Run this function
-	#add_16_ppa
-	install_16_packages
-	#configure_16
-	#install_gchrome
 
-elif [[ `lsb_release -rs` == "14.04" ]] # for Ubuntu 14.04
-then
 
-	#Run this function
-	echo $HOME
-
-else
-	echo "OS not currently supported"
-fi
